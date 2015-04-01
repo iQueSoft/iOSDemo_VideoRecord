73cf338528249a8be49923b7dfd05801e5273da1
Ruslan Shevtsov <r.shevtsov@aericon.com>
2015-04-01 12:45:54 +0300

    Initial Commit

---
+++ VideoRecordSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoRecordSample
+//
+//  Created by Ruslan Shevtsov on 4/1/15.
+//  Copyright (c) 2015 Work. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoRecordSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoRecordSample
+//
+//  Created by Ruslan Shevtsov on 4/1/15.
+//  Copyright (c) 2015 Work. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
